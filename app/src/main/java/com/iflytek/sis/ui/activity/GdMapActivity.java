b19549411144eb56de7e64093eb1a2f8655f12f9
stzhang3 <stzhang3@iflytek.com>
2018-11-09 10:28:53 +0800

    [地图模块]增加高德地图模块

--- app/src/main/java/com/iflytek/sis/ui/activity/GdMapActivity.java
+++ app/src/main/java/com/iflytek/sis/ui/activity/GdMapActivity.java
@@ -2,16 +2,20 @@ package com.iflytek.sis.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.iflytek.sis.R;
 
 public class GdMapActivity extends AppCompatActivity {
 
     MapView mapView = null;
+    AMap aMap = null;
+    MyLocationStyle myLocationStyle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,24 @@ public class GdMapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gd_map);
         mapView = findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
-        AMap aMap = null;
         if (aMap == null){
             aMap = mapView.getMap();
         }
+        myLocationStyle = new MyLocationStyle();
+        //设置连续定位模式下的定位间隔，单位毫秒
+        myLocationStyle.interval(2000);
+        aMap.setMyLocationStyle(myLocationStyle);
+        //设置是否显示定位按钮
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
+        aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                //从location对象中获取经纬度信息，地址描述信息
+            }
+        });
+        aMap.showIndoorMap(true);
     }
 
     @Override
